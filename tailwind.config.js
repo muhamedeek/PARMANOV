e14f76f4474d2b723d4a51e2d140abb4068c7f83
muhamedeek <66794679+muhamedeek@users.noreply.github.com>
2025-09-16 16:08:21 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/**** Tailwind config ****/
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
